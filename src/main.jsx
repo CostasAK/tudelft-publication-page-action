ef25b7119674cf023d80e2b43cc142f91a8476f8
CostasAK <costas@kokke.eu>
2023-12-15 22:58:14 +0100

    fix: render to root div instead of to body directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import "./index.css";
 
-import { Root } from "./layout/Root";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Root } from "./layout/Root";
 
-const rootElement = document.getElementById("body");
+const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
   <StrictMode>
