ddc304e01e478fceb7b44d6813a8caefad287885
CostasAK <costas@kokke.eu>
2023-12-15 22:59:26 +0100

    feat: set header font size and alignment

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,15 @@ import clsx from "clsx";
 
 export const Header = () => (
   <header
-    className={clsx("py-12", "px-8", "bg-cyan", "font-serif", "text-white")}
+    className={clsx(
+      "py-12",
+      "px-8",
+      "bg-cyan",
+      "font-serif",
+      "text-white",
+      "text-5xl",
+      "text-center",
+    )}
   >
     <h1>{import.meta.env.VITE_APP_NAME}</h1>
   </header>
