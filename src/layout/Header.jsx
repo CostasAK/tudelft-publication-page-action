ac9af01b171dcac2e2046f5cf4fe0a60642f2249
CostasAK <costas@kokke.eu>
2023-12-15 23:38:57 +0100

    refactor(Header): apply text syle to h1 instead of header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,17 +1,9 @@
 import clsx from "clsx";
 
 export const Header = () => (
-  <header
-    className={clsx(
-      "py-12",
-      "px-8",
-      "bg-cyan",
-      "font-serif",
-      "text-white",
-      "text-5xl",
-      "text-center",
-    )}
-  >
-    <h1>{import.meta.env.VITE_APP_NAME}</h1>
+  <header className={clsx("py-12", "px-8", "bg-cyan")}>
+    <h1 className={clsx("font-serif", "text-white", "text-5xl", "text-center")}>
+      {import.meta.env.VITE_APP_NAME}
+    </h1>
   </header>
 );
