6e576263bb36aa689e5508ab83a9bde1a02f7289
CostasAK <costas@kokke.eu>
2023-12-15 23:10:09 +0100

    build(vite): include .bib assets

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
+  assetsInclude: ["**/*.bib"],
   plugins: [react(), svgrPlugin(), ViteImageOptimizer({ test: /\.svg$/i })],
   test: { environment: "happy-dom" },
 });
