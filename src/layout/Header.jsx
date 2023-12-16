86f00a93d2b1813a3fffe1dec85760e07d6e76cb
CostasAK <costas@kokke.eu>
2023-12-16 16:55:03 +0100

    feat: add authors to header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,15 +5,30 @@ import { cslJson } from "../constants/cslJson";
 export const Header = () => {
   const title = cslJson.title;
   return (
-    <header className={clsx("py-12", "px-8", "bg-cyan")}>
+    <header
+      className={clsx("py-12", "px-8", "bg-cyan", "text-white", "text-center")}
+    >
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <h1
-        className={clsx("font-serif", "text-white", "text-5xl", "text-center")}
+      <h1 className={clsx("font-serif", "text-5xl")}>{title}</h1>
+      <div
+        className={clsx(
+          "text-xl",
+          "flex",
+          "flex-row",
+          "flex-wrap",
+          "justify-around",
+          "mt-6",
+          "gap-12",
+        )}
       >
-        {title}
-      </h1>
+        {cslJson.author.map((author, i) => (
+          <div key={i}>
+            {author.given} {author.family}
+          </div>
+        ))}
+      </div>
     </header>
   );
 };
