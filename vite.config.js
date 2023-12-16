0167cbd0fcfbc98f32d95a4a54055dc5505f02f3
CostasAK <costas@kokke.eu>
2023-12-16 14:29:05 +0100

    feat: install vite-plugin-webfont-dl for Roboto Slab Medium

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import svgrPlugin from "vite-plugin-svgr";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
   assetsInclude: ["**/*.bib"],
-  plugins: [react(), svgrPlugin(), ViteImageOptimizer({ test: /\.svg$/i })],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    ViteImageOptimizer({ test: /\.svg$/i }),
+    webfontDownload(),
+  ],
   test: { environment: "happy-dom" },
 });
