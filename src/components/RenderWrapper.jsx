4d70782daa07fc5d197f3b45682394dc167f16bf
CostasAK <costas@kokke.eu>
2023-12-16 15:07:23 +0100

    revert: uninstall react-query and axios

--- src/components/RenderWrapper.jsx
+++ src/components/RenderWrapper.jsx
@@ -1,14 +1,8 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PropTypes from "prop-types";
 import { HelmetProvider } from "react-helmet-async";
-import { queryClient } from "../utils/queryClient";
 
 export const RenderWrapper = ({ children }) => (
-  <QueryClientProvider client={queryClient}>
-    <HelmetProvider>{children}</HelmetProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <HelmetProvider>{children}</HelmetProvider>
 );
 
 RenderWrapper.propTypes = {
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RenderWrapper } from "./components/RenderWrapper";
 import { Root } from "./layout/Root";
-import { queryClient } from "./utils/queryClient";
 
 const rootElement = document.getElementById("root");
-
-queryClient.prefetchQuery({ queryKey: ["bib"] });
 
 createRoot(rootElement).render(
   <StrictMode>
